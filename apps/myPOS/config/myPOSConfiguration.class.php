21e6ffa173deb7675527b584f5205337cfdcda06
james k <jamesk525@gmail.com>
2014-04-07 09:43:39 -0400

    no message

---
+++ apps/myPOS/config/myPOSConfiguration.class.php
@@ -0,0 +1,8 @@
+<?php
+
+class myPOSConfiguration extends sfApplicationConfiguration
+{
+  public function configure()
+  {
+  }
+}
---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('myPOS', 'prod', false);sfContext::createInstance($configuration)->dispatch();
+
+?>
